2d515985f11b16e4e3273b949525e0786979171d
MustufaMemon10 <mustufasam06@gmail.com>
2024-03-16 00:47:53 +0530

    home screen if has card checked

--- lib/Widgets/Home Widget.dart
+++ lib/Widgets/Home Widget.dart
@@ -6,6 +6,10 @@ import 'package:petrocardapppp/Components/bottom_navigationbar.dart';
 import 'package:petrocardapppp/screens/MainScreen/CardScreen.dart';
 import 'package:petrocardapppp/screens/MainScreen/HomeScreen.dart';
 import 'package:petrocardapppp/screens/MainScreen/Location_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../screens/API/ApiHelper.dart';
+import '../screens/Admin side/Request_Card/RequestCardScreen.dart';
 
 class HomeWidget extends StatefulWidget {
   final bool isDrawerOpen;
@@ -25,8 +29,26 @@ class HomeWidget extends StatefulWidget {
 
 class HomeWidgetState extends State<HomeWidget> {
   int _currentIndex = 1;
-  bool hasCard = false;
-
+  bool hasCard = true;
+  String? userId;
+  Future<void> checkHasCard(String id) async {
+    bool cardStatus = await ApiHelper.checkHasCard(userId!);
+    setState(() {
+      hasCard = cardStatus;
+    });
+  }
+  @override
+  void initState() {
+    super.initState();
+    getId();
+  }
+  Future<void> getId() async {
+    SharedPreferences setpreference = await SharedPreferences.getInstance();
+    setState(() {
+      userId = setpreference.getString('id') ?? '';
+    });
+    await checkHasCard(userId!);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,13 +94,13 @@ class HomeWidgetState extends State<HomeWidget> {
       if (hasCard) {
         return const HomeScreen();
       } else {
-        return const Request_Screen();
+        return const RequestCardScreen(); // Change to check request screen
       }
     } else if (_currentIndex == 2) {
       if (hasCard) {
         return const CardScreen();
       } else {
-        return const Request_Screen();
+        return const RequestCardScreen();
       }
     }
     return Container(); // Return an empty container if index is not 0, 1, or 2
