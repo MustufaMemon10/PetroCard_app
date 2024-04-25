36f8efb03e90cf9ad93559b220eecbb90e125882
MustufaMemon10 <mustufasam06@gmail.com>
2024-04-25 23:35:56 +0530

    changes done

--- lib/DrawerComponents/AboutUspage.dart
+++ lib/DrawerComponents/AboutUspage.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -43,81 +44,84 @@ class AboutUsPage extends StatelessWidget {
       ),
       body:SingleChildScrollView(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Welcome to Petro Card',
-              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'At Petro Card, we are dedicated to revolutionizing the way you manage your fuel expenses and travel experiences. Our innovative petro card system empowers users with convenience, security, and savings every time they fuel up.',
-              style: TextStyle(fontSize: 16.0),
-            ),
-            SizedBox(height: 24.0),
-            Text(
-              'Our Mission',
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Our mission is to simplify and enhance the fueling experience for individuals and businesses alike. We strive to provide a seamless and efficient petro card solution that meets the diverse needs of our users while promoting sustainability and environmental responsibility.',
-              style: TextStyle(fontSize: 16.0),
-            ),
-            SizedBox(height: 24.0),
-            Text(
-              'Key Features',
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16.0),
-            _buildFeature('Convenient Fueling',
-                'With our petro card, users can easily pay for fuel at thousands of participating gas stations nationwide, eliminating the hassle of carrying cash or multiple credit cards.'),
-            _buildFeature('Enhanced Security',
-                'We prioritize the security of our users\' accounts and transactions. Our petro card features advanced security measures such as biometric authentication and real-time fraud monitoring to ensure peace of mind.'),
-            _buildFeature('Savings and Rewards',
-                'Enjoy exclusive discounts, loyalty rewards, and personalized offers with our petro card loyalty program. Earn points for every fuel purchase and redeem them for discounts, free fuel, or partner rewards.'),
-            _buildFeature('User-Friendly Mobile App',
-                'Our dedicated mobile app provides users with seamless account management, transaction tracking, and fuel station locator features on the go. Stay informed and in control of your fueling activities anytime, anywhere.'),
-            _buildFeature('Environmental Initiatives',
-                'We are committed to environmental sustainability. Through our petro card system, users can track their carbon footprint, offset emissions, and participate in eco-friendly fueling initiatives.'),
-            SizedBox(height: 24.0),
-            Text(
-              'Get Started Today',
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Join the Petro Card community and experience the future of fueling. Whether you\'re an individual driver or a business with a fleet of vehicles, our petro card system is designed to meet your needs and exceed your expectations.',
-              style: TextStyle(fontSize: 16.0),
-            ),
-            SizedBox(height: 24.0),
-            Text(
-              'Contact Us',
-              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              'Have questions or feedback? We\'re here to help! Reach out to our customer support team for assistance with your petro card account or any inquiries about our application.',
-              style: TextStyle(fontSize: 16.0),
-            ),
-            SizedBox(height: 16.0),
-            TextButton(
-              onPressed: (){_launchPhoneDialer('+1234567890'); },
-              child: Row(
-                children: [
-                  Text(
-                    'Contact Number: ',
-                    style: TextStyle(fontSize: 16.0,color: AppColors.darkPurple),
-                  ),
-                  Text(
-                    '+123-45-6789',
-                    style: TextStyle(fontSize: 16.0),
-                  ),
-                ],
+        child:  FadeInDown(
+          duration: Duration(milliseconds: 1000),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Welcome to Petro Card',
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              SizedBox(height: 16.0),
+              Text(
+                'At Petro Card, we are dedicated to revolutionizing the way you manage your fuel expenses and travel experiences. Our innovative petro card system empowers users with convenience, security, and savings every time they fuel up.',
+                style: TextStyle(fontSize: 16.0),
+              ),
+              SizedBox(height: 24.0),
+              Text(
+                'Our Mission',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Our mission is to simplify and enhance the fueling experience for individuals and businesses alike. We strive to provide a seamless and efficient petro card solution that meets the diverse needs of our users while promoting sustainability and environmental responsibility.',
+                style: TextStyle(fontSize: 16.0),
+              ),
+              SizedBox(height: 24.0),
+              Text(
+                'Key Features',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16.0),
+              _buildFeature('Convenient Fueling',
+                  'With our petro card, users can easily pay for fuel at thousands of participating gas stations nationwide, eliminating the hassle of carrying cash or multiple credit cards.'),
+              _buildFeature('Enhanced Security',
+                  'We prioritize the security of our users\' accounts and transactions. Our petro card features advanced security measures such as biometric authentication and real-time fraud monitoring to ensure peace of mind.'),
+              _buildFeature('Savings and Rewards',
+                  'Enjoy exclusive discounts, loyalty rewards, and personalized offers with our petro card loyalty program. Earn points for every fuel purchase and redeem them for discounts, free fuel, or partner rewards.'),
+              _buildFeature('User-Friendly Mobile App',
+                  'Our dedicated mobile app provides users with seamless account management, transaction tracking, and fuel station locator features on the go. Stay informed and in control of your fueling activities anytime, anywhere.'),
+              _buildFeature('Environmental Initiatives',
+                  'We are committed to environmental sustainability. Through our petro card system, users can track their carbon footprint, offset emissions, and participate in eco-friendly fueling initiatives.'),
+              SizedBox(height: 24.0),
+              Text(
+                'Get Started Today',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Join the Petro Card community and experience the future of fueling. Whether you\'re an individual driver or a business with a fleet of vehicles, our petro card system is designed to meet your needs and exceed your expectations.',
+                style: TextStyle(fontSize: 16.0),
+              ),
+              SizedBox(height: 24.0),
+              Text(
+                'Contact Us',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Have questions or feedback? We\'re here to help! Reach out to our customer support team for assistance with your petro card account or any inquiries about our application.',
+                style: TextStyle(fontSize: 16.0),
+              ),
+              SizedBox(height: 16.0),
+              TextButton(
+                onPressed: (){_launchPhoneDialer('+1234567890'); },
+                child: Row(
+                  children: [
+                    Text(
+                      'Contact Number: ',
+                      style: TextStyle(fontSize: 16.0,color: AppColors.darkPurple),
+                    ),
+                    Text(
+                      '+123-45-6789',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/Admin side/Complain/add_complaints_page.dart
+++ lib/screens/Admin side/Complain/add_complaints_page.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:petrocardapppp/utilities/colors.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
@@ -49,73 +51,96 @@ class _AddComplaintsPageState extends State<AddComplaintsPage> {
           SizedBox(width: 20,),
         ],
       ),
-      body: isLoading ? Center(child: CircularProgressIndicator(color: Theme.of(context).primaryColor)) : SingleChildScrollView(
-        child: Form(
-          key: formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 20,),
-              Padding(padding: EdgeInsets.all(16),
-                child:  Text(
-                  "What's your Complaints",
-                  style:
-                  TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.left,
-                ),),
-              Container(
-                padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
-                child: TextFormField(
-                  style: TextStyle(color: Color(0xFF08364B)),
-                  validator: (val) {
-                    if (val!.isEmpty) {
-                      return "Please enter message";
-                    }
-                    return null;
-                  },
-                  controller: messageController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Message',
-                    labelStyle:TextStyle(color: Color(0xFF08364B)),
-                    helperStyle: TextStyle(color: Color(0xFF08364B)),
-                    hintStyle: TextStyle(color: Color(0xFF08364B)),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16.0),
-                      borderSide: BorderSide(
-                        color: Color(0xFF08364B),
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16.0),
-                      borderSide: BorderSide(
-                        color: Color(0xFF08364B),
-                        width: 2.0,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 20,),
+                  FadeInDown(
+                    duration: Duration(milliseconds: 550),
+                    child: Padding(padding: EdgeInsets.all(16),
+                      child:  Text(
+                        "What's your Complaint?",
+                        style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left,
+                      ),),
+                  ),
+                  FadeInDown(
+                    duration: Duration(milliseconds: 600),
+                    child: Container(
+                      padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+                      child: TextFormField(
+                        style: TextStyle(color: Color(0xFF08364B)),
+                        validator: (val) {
+                          if (val!.isEmpty) {
+                            return "Please enter message";
+                          }
+                          return null;
+                        },
+                        controller: messageController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Message',
+                          labelStyle:TextStyle(color: Color(0xFF08364B)),
+                          helperStyle: TextStyle(color: Color(0xFF08364B)),
+                          hintStyle: TextStyle(color: Color(0xFF08364B)),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                            borderSide: BorderSide(
+                              color: Color(0xFF08364B),
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(16.0),
+                            borderSide: BorderSide(
+                              color: Color(0xFF08364B),
+                              width: 2.0,
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                  SizedBox(height:20 ),
+                  FadeInDown(
+                    duration: Duration(milliseconds: 650),
+                    child: Container(
+                        height: 55,
+                        width: double.infinity,
+                        padding: EdgeInsets.only(left: 16,right: 16),
+                        margin: EdgeInsets.all(10),
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              backgroundColor: AppColors.darkPurple,
+                              side: BorderSide.none,
+                              shape: StadiumBorder(),
+                            ),
+                            child: const Text('Send Complaint',
+                              style: TextStyle(color: Colors.white),),
+                            onPressed: _submit
+                        )),
+                  ),
+                ],
               ),
-              SizedBox(height:20 ),
-              Container(
-                  height: 55,
-                  width: double.infinity,
-                  padding: EdgeInsets.only(left: 16,right: 16),
-                  margin: EdgeInsets.all(10),
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        elevation: 0,
-                        backgroundColor: AppColors.darkPurple,
-                        side: BorderSide.none,
-                        shape: StadiumBorder(),
-                      ),
-                      child: const Text('Send Complaint',
-                        style: TextStyle(color: Colors.white),),
-                      onPressed: _submit
-                  )),
-            ],
+            ),
           ),
-        ),
+          Positioned.fill(
+            child: isLoading
+                ? Container(
+                color: AppColors.white.withOpacity(0.5),
+                child: Center(
+                  child: LoadingAnimationWidget.halfTriangleDot(
+                      color: AppColors.darkPurple, size: 50),
+                ))
+                : SizedBox(),
+          ),
+        ],
       ) ,
     );
   }
